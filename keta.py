3d370a789d0fa46070b99db348c385dd8a5961e4
Elgin Akin <63241361+elginakin@users.noreply.github.com>
2023-01-21 13:54:04 -0500

    Update keta.py

--- keta.py
+++ keta.py
@@ -1,9 +1,5 @@
 #keta 
 
-import numpy as np
-import pandas as pd
-import math
-
 prompt = int(input("Enter the provided time given by K: \n in hours: "))
 #query1 = input("In K's provided estimation, were the words: 'likey' or 'probobly' used?  \n (yes or no):  ")
 #query2 = input("Is it a weekend or weekday? (enter one option as prompted in the question) \n (yes or no): ")
